193c990c01f65e71a5783af59e1f410d9e5c0b50
lzk2023 <138744375+lzk2023@users.noreply.github.com>
2024-11-26 18:23:00 +0800

    Update SA.v

--- SA.v
+++ SA.v
@@ -40,11 +40,30 @@ reg                             state     ;
 reg                             x_vld     ;
 reg                             w_vld     ;
 reg                             d_vld     ;
-wire       [S*64*16-1:0]        data_i_o  ;
-wire       [S*64*16-1:0]        x_i_o     ;
+
+wire [15:0] i_w_matrix      [0:S-1] [0:63];
+wire [15:0] data_io_matrix  [0:S-1] [0:63];
+wire [15:0] x_io_matrix     [0:S-1] [0:63];
+
+//wire       [S*64*16-1:0]        data_i_o  ;
+//wire       [S*64*16-1:0]        x_i_o     ;
 wire                            pe00_vld  ;
 
-assign O_OUT = data_i_o[S*64*16-1:(S-1)*64*16];
+generate 
+    for(genvar i=0;i<S;i=i+1)begin
+        for(genvar j=0;j<64;j=j+1)begin
+            assign i_w_matrix[i][j] = I_W[(i*64+j)*16 +: 16];
+            //assign data_io_matrix[i][j] = data_i_o[(i*64+j)*16 +: 16];
+            //assign x_io_matrix [i][j] = x_i_o[(i*64+j)*16 +: 16];
+        end
+    end
+endgenerate
+
+generate 
+    for(genvar j=0;j<64;j=j+1)begin
+        assign O_OUT[j*16 +: 16] = data_io_matrix[S-1][j];//data_i_o[S*64*16-1:(S-1)*64*16];
+    end
+endgenerate
 assign O_SHIFT = pe00_vld;
 
 always@(posedge I_CLK or negedge I_RST_N)begin
@@ -87,76 +106,76 @@ always@(*)begin
         d_vld = 0;
     end
 end
-                                                        //   SA:                   64
+                                                        //   SA:                   64(j)
 genvar i;                                               //       x|<--------------------------------->|
 genvar j;                                               //       |
-generate                                                //     S |               S x 64
-    for(j=0;j<S;j=j+1)begin                             //       |
-        for(i=0;i<64;i=i+1)begin                        //       x
+generate                                                //  S(i) |               S x 64
+    for(i=0;i<S;i=i+1)begin                             //       |
+        for(j=0;j<64;j=j+1)begin                        //       x
             if(i == 0 & j == 0)begin
                 PE u_PE(
                     .I_CLK     (I_CLK  ),
                     .I_RST_N   (I_RST_N),
                     .I_X_VLD   (x_vld),
-                    .I_X       (I_X[j*16 +: 16]),//input x(from left)
+                    .I_X       (I_X[0 +: 16]),//input x(from left)
                     .I_W_VLD   (w_vld),
-                    .I_W       (I_W[(j*64+i)*16 +: 16]),//input weight(from ddr)
+                    .I_W       (i_w_matrix[0][0]),//input weight(from ddr)
                     .I_D_VLD   (d_vld),
                     .I_D       (16'b0),//input data(from up)
                     .O_X_VLD   (),
-                    .O_X       (x_i_o[j*64*16 +: 16]),//output x(right shift)
+                    .O_X       (x_io_matrix[0][0]),//output x(right shift)
                     .O_MUL_DONE(),//multiply done,next clk add,ID_VLD <=0
                     .O_OUT_VLD (pe00_vld),
-                    .O_OUT     (data_i_o[(j*64+i)*16 +: 16])//output data(down shift)
+                    .O_OUT     (data_io_matrix[0][0])//output data(down shift)
                 );
             end
-            else if(i == 0 & j != 0)begin
+            else if(i != 0 & j == 0)begin
                 PE u_PE(
                     .I_CLK     (I_CLK  ),
                     .I_RST_N   (I_RST_N),
                     .I_X_VLD   (x_vld),
-                    .I_X       (I_X[j*16 +: 16]),//input x(from left)
+                    .I_X       (I_X[i*16 +: 16]),//input x(from left)
                     .I_W_VLD   (w_vld),
-                    .I_W       (I_W[(j*64+i)*16 +: 16]),//input weight(from ddr)
+                    .I_W       (i_w_matrix[i][0]),//input weight(from ddr)
                     .I_D_VLD   (d_vld),
-                    .I_D       (data_i_o[((j-1)*64+i)*16 +: 16]),//input data(from up)
+                    .I_D       (data_io_matrix[i-1][0]),//input data(from up)
                     .O_X_VLD   (),
-                    .O_X       (x_i_o[j*64*16 +: 16]),//output x(right shift)
+                    .O_X       (x_io_matrix[i][0]),//output x(right shift)
                     .O_MUL_DONE(),//multiply done,next clk add,ID_VLD <=0
                     .O_OUT_VLD (),
-                    .O_OUT     (data_i_o[(j*64+i)*16 +: 16])//output data(down shift)
+                    .O_OUT     (data_io_matrix[i][0])//output data(down shift)
                 );
-            end else if(i != 0 & j == 0)begin
+            end else if(i == 0 & j != 0)begin
                 PE u_PE(
                     .I_CLK     (I_CLK  ),
                     .I_RST_N   (I_RST_N),
                     .I_X_VLD   (x_vld),
-                    .I_X       (x_i_o[(j*64+(i-1))*16 +: 16]),//input x(from left)
+                    .I_X       (x_io_matrix[0][j-1]),//input x(from left)
                     .I_W_VLD   (w_vld),
-                    .I_W       (I_W[(j*64+i)*16 +: 16]),//input weight(from ddr)
+                    .I_W       (i_w_matrix[0][j]),//input weight(from ddr)
                     .I_D_VLD   (d_vld),
                     .I_D       (16'b0),//input data(from up)
                     .O_X_VLD   (),
-                    .O_X       (x_i_o[(j*64+i)*16 +: 16]),//output x(right shift)
+                    .O_X       (x_io_matrix[0][j]),//output x(right shift)
                     .O_MUL_DONE(),//multiply done,next clk add,ID_VLD <=0
                     .O_OUT_VLD (),
-                    .O_OUT     (data_i_o[(j*64+i)*16 +: 16])//output data(down shift)
+                    .O_OUT     (data_io_matrix[0][j])//output data(down shift)
                 );
             end else begin
                 PE u_PE(
                     .I_CLK     (I_CLK  ),
                     .I_RST_N   (I_RST_N),
                     .I_X_VLD   (x_vld),
-                    .I_X       (x_i_o[(j*64+(i-1))*16 +: 16]),//input x(from left)
+                    .I_X       (x_io_matrix[0][j-1]),//input x(from left)
                     .I_W_VLD   (w_vld),
-                    .I_W       (I_W[(j*64+i)*16 +: 16]),//input weight(from ddr)
+                    .I_W       (i_w_matrix[i][j]),//input weight(from ddr)
                     .I_D_VLD   (d_vld),
-                    .I_D       (data_i_o[((j-1)*64+i)*16 +: 16]),//input data(from up)
+                    .I_D       (data_io_matrix[i-1][j]),//input data(from up)
                     .O_X_VLD   (),
-                    .O_X       (x_i_o[(j*64+i)*16 +: 16]),//output x(right shift)
+                    .O_X       (x_i_o[(i*64+j)*16 +: 16]),//output x(right shift)
                     .O_MUL_DONE(),//multiply done,next clk add,ID_VLD <=0
                     .O_OUT_VLD (),
-                    .O_OUT     (data_i_o[(j*64+i)*16 +: 16])//output data(down shift)
+                    .O_OUT     (data_io_matrix[i][j])//output data(down shift)
                 );
             end
         end

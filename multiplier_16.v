5d6dac75221535f05abc24db96babece128eb679
lzk2023 <138744375+lzk2023@users.noreply.github.com>
2024-11-29 11:22:24 +0800

    Update multiplier_16.v

--- multiplier_16.v
+++ multiplier_16.v
@@ -30,66 +30,47 @@ output reg         O_MUL_BUSY,
 output     [15:0]  O_PRODUCT  //product     (积)
 );
 
-wire    [14:0]  ab_m1     ;
-wire    [14:0]  ab_m2     ;//calculate m1,m2 absolute value(用于计算m1m2绝对值)
-wire    [29:0]  product_reg_n;
-wire    [29:0]  prod_shift;
-wire    [14:0]  o_product_sel;
+wire    [31:0]  prod_shift;
+wire    [31:0]  prod_32   ;
 
-reg             prd_msb   ;//product msb   积符号位
-reg     [29:0]  product_reg;//15 bits * 15bits
-reg     [29:0]  ab_m1_reg ;
+reg     [31:0]  product_reg;//15 bits * 15bits
+reg     [31:0]  ab_m1_reg ;
 reg     [14:0]  ab_m2_reg ;//reg input(absolute value)  寄存输入数据绝对值
+reg             ab_m2_msb ;
 reg     [1:0]   mul_cycle ;//
 
-assign ab_m1 = I_M1[15] ? ~I_M1[14:0] + 1 : I_M1[14:0];
-assign ab_m2 = I_M2[15] ? ~I_M2[14:0] + 1 : I_M2[14:0];//calculate m1,m2 absolute value(用于计算m1m2绝对值)
-assign product_reg_n = ~product_reg + 1;
-assign o_product_sel = prd_msb ? product_reg_n[27:13] : product_reg[27:13];
-//assign O_VLD = (O_MUL_BUSY & ab_m2_reg == 0) ? 1 : 0;
 assign O_VLD = (O_MUL_BUSY & mul_cycle == 3) ? 1 : 0;
-assign O_PRODUCT = {prd_msb,o_product_sel};//select output 选择输出
+assign O_PRODUCT = {prod_32[31],prod_32[27:13]};
+assign prod_32 = product_reg - (ab_m2_reg[15] ? ab_m1_reg<<1 : 0);//select output 选择输出
 
 always@(posedge I_CLK or negedge I_RST_N)begin
     if(!I_RST_N)begin
         ab_m1_reg  <= 'b0;
         ab_m2_reg  <= 'b0;
+        ab_m2_msb  <= 'b0;
         O_MUL_BUSY <=   0;
         mul_cycle  <=   0;
     end else if(I_VLD & !O_MUL_BUSY)begin          //clk 1:calculate and store m1,m2 absolute value 计算绝对值并寄存
-        ab_m1_reg  <= ab_m1;
-        ab_m2_reg  <= ab_m2;
-        O_MUL_BUSY <=     1;
-        mul_cycle  <=     0;
+        ab_m1_reg  <= {16'b0,I_M1};
+        ab_m2_reg  <= I_M2[14:0]  ;
+        ab_m2_msb  <= I_M2[15]    ;
+        O_MUL_BUSY <=           1 ;
+        mul_cycle  <=           0 ;
     //end else if(O_MUL_BUSY & ab_m2_reg != 0)begin
     end else if(O_MUL_BUSY & mul_cycle < 3)begin
         ab_m1_reg <= ab_m1_reg << 5;                          //clk 2/3/4:pipeline_3
         ab_m2_reg <= ab_m2_reg >> 5;
+        ab_m2_msb <= ab_m2_msb     ;
         mul_cycle <= mul_cycle + 1 ;
     end else begin
         ab_m1_reg   <=     'b0;
         ab_m2_reg   <=     'b0;
+        ab_m2_msb   <=       0;
         O_MUL_BUSY  <=       0;
-        mul_cycle  <=        0;
+        mul_cycle   <=       0;
     end
 end
-always@(posedge I_CLK or negedge I_RST_N)begin
-    if(!I_RST_N)begin
-        prd_msb    <=   0;
-    end else if(I_VLD & !O_MUL_BUSY)begin
-        if(I_M1 == 0 |I_M2 == 0)begin
-            prd_msb    <= 0;
-        end else if(I_M1 == 16'b1000_0000_0000_0000 | I_M2 == 16'b1000_0000_0000_0000)begin
-            prd_msb    <= 0;
-        end else begin
-            prd_msb    <= I_M1[15] ^ I_M2[15];
-        end
-    end else if(O_MUL_BUSY & mul_cycle < 3)begin
-        prd_msb    <= prd_msb;
-    end else begin
-        prd_msb     <=  0;
-    end
-end
+
 always@(posedge I_CLK or negedge I_RST_N)begin
     if(!I_RST_N)begin
         product_reg <= 'b0;

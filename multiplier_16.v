c3b4fbfd2d4bf9dad7e6cdec72ec5fa68d8846bb
lzk2023 <138744375+lzk2023@users.noreply.github.com>
2024-11-29 18:12:38 +0800

    Update multiplier_16.v

--- multiplier_16.v
+++ multiplier_16.v
@@ -33,13 +33,15 @@ output     [15:0]  O_PRODUCT  //product     (积)
 wire    [31:0]  prod_shift;
 wire    [31:0]  prod_32   ;
 wire    [31:0]  o_m1_sft5 ;
+wire    [14:0]  m2_15     ;//reg input  寄存输入数据
+wire            m2_msb    ;
 
 reg     [31:0]  product_reg;//15 bits * 15bits
 reg     [31:0]  m1_reg ;
-reg     [14:0]  m2_reg ;//reg input  寄存输入数据
-reg             m2_msb ;
 reg     [1:0]   mul_cycle ;//
 
+assign m2_15     = I_M2[14:0];
+assign m2_msb    = I_M2[15];
 assign O_VLD     = (O_MUL_BUSY & mul_cycle == 3) ? 1 : 0;
 assign O_PRODUCT = {prod_32[31],prod_32[27:13]};
 assign prod_32   = product_reg - (m2_msb ? m1_reg<<1 : 0);//select output 选择输出
@@ -47,26 +49,18 @@ assign prod_32   = product_reg - (m2_msb ? m1_reg<<1 : 0);//select output 选择
 always@(posedge I_CLK or negedge I_RST_N)begin
     if(!I_RST_N)begin
         m1_reg     <= 'b0;
-        m2_reg     <= 'b0;
-        m2_msb     <= 'b0;
         O_MUL_BUSY <=   0;
         mul_cycle  <=   0;
     end else if(I_VLD & !O_MUL_BUSY)begin          //
         m1_reg     <= {{16{I_M1[15]}},I_M1};
-        m2_reg     <= I_M2[14:0]  ;
-        m2_msb     <= I_M2[15]    ;
         O_MUL_BUSY <=           1 ;
         mul_cycle  <=           0 ;
     //end else if(O_MUL_BUSY & ab_m2_reg != 0)begin
     end else if(O_MUL_BUSY & mul_cycle < 3)begin
         m1_reg    <= o_m1_sft5     ;                          //clk 2/3/4:pipeline_3
-        m2_reg    <= m2_reg        ;
-        m2_msb    <= m2_msb        ;
         mul_cycle <= mul_cycle + 1 ;
     end else begin
         m1_reg      <=     'b0;
-        m2_reg      <=     'b0;
-        m2_msb      <=       0;
         O_MUL_BUSY  <=       0;
         mul_cycle   <=       0;
     end

77834dcf32003da1878001da2c15f08321445a6e
lzk2023 <138744375+lzk2023@users.noreply.github.com>
2024-12-03 15:27:52 +0800

    Update multiplier_16.v

--- multiplier_16.v
+++ multiplier_16.v
@@ -30,42 +30,43 @@ output reg         O_MUL_BUSY,
 output     [15:0]  O_PRODUCT  //product     (积)
 );
 
-wire    [31:0]  prod_shift;
-wire    [31:0]  prod_32   ;
+wire    [31:0]  prod_shift ;
+wire    [31:0]  prod_32    ;
+wire    [31:0]  m1_reg_sft5;
 
-reg     [31:0]  product_reg;//15 bits * 15bits
-reg     [31:0]  ab_m1_reg ;
-reg     [14:0]  ab_m2_reg ;//reg input(absolute value)  寄存输入数据绝对值
-reg             ab_m2_msb ;
-reg     [1:0]   mul_cycle ;//
+reg     [31:0]  product_reg;//16 bits * 16bits
+reg     [31:0]  m1_reg     ;
+reg     [14:0]  m2_reg     ;//reg input(absolute value)  寄存输入数据
+reg             m2_msb     ;
+reg     [1:0]   mul_cycle  ;//
 
 assign O_VLD = (O_MUL_BUSY & mul_cycle == 3) ? 1 : 0;
 assign O_PRODUCT = {prod_32[31],prod_32[27:13]};
-assign prod_32 = product_reg - (ab_m2_msb ? ab_m1_reg<<1 : 0);//select output 选择输出
+assign prod_32 = product_reg - (m2_msb ? m1_reg : 0);//select output 选择输出
 
 always@(posedge I_CLK or negedge I_RST_N)begin
     if(!I_RST_N)begin
-        ab_m1_reg  <= 'b0;
-        ab_m2_reg  <= 'b0;
-        ab_m2_msb  <= 'b0;
+        m1_reg     <= 'b0;
+        m2_reg     <= 'b0;
+        m2_msb     <= 'b0;
         O_MUL_BUSY <=   0;
         mul_cycle  <=   0;
-    end else if(I_VLD & !O_MUL_BUSY)begin          //clk 1:calculate and store m1,m2 absolute value 计算绝对值并寄存
-        ab_m1_reg  <= {{16{I_M1[15]}},I_M1};
-        ab_m2_reg  <= I_M2[14:0]  ;
-        ab_m2_msb  <= I_M2[15]    ;
+    end else if(I_VLD & !O_MUL_BUSY)begin          //clk 1:
+        m1_reg     <= {{16{I_M1[15]}},I_M1};
+        m2_reg     <= I_M2[14:0]  ;
+        m2_msb     <= I_M2[15]    ;
         O_MUL_BUSY <=           1 ;
         mul_cycle  <=           0 ;
     //end else if(O_MUL_BUSY & ab_m2_reg != 0)begin
     end else if(O_MUL_BUSY & mul_cycle < 3)begin
-        ab_m1_reg <= ab_m1_reg << 5;                          //clk 2/3/4:pipeline_3
-        ab_m2_reg <= ab_m2_reg >> 5;
-        ab_m2_msb <= ab_m2_msb     ;
-        mul_cycle <= mul_cycle + 1 ;
+        m1_reg     <= m1_reg_sft5   ;                          //clk 2/3/4:pipeline_3
+        m2_reg     <= m2_reg >> 5   ;
+        m2_msb     <= m2_msb        ;
+        mul_cycle  <= mul_cycle + 1 ;
     end else begin
-        ab_m1_reg   <=     'b0;
-        ab_m2_reg   <=     'b0;
-        ab_m2_msb   <=       0;
+        m1_reg      <=     'b0;
+        m2_reg      <=     'b0;
+        m2_msb      <=       0;
         O_MUL_BUSY  <=       0;
         mul_cycle   <=       0;
     end
@@ -74,7 +75,7 @@ end
 always@(posedge I_CLK or negedge I_RST_N)begin
     if(!I_RST_N)begin
         product_reg <= 'b0;
-    //end else if(O_MUL_BUSY & ab_m2_reg != 0)begin
+    //end else if(O_MUL_BUSY & m2_reg != 0)begin
     end else if(O_MUL_BUSY & mul_cycle < 3)begin
         product_reg <= product_reg + prod_shift;  //add shift 累加移位结果   
     end else begin
@@ -82,8 +83,9 @@ always@(posedge I_CLK or negedge I_RST_N)begin
     end
 end
 mul_shift5 u_mul6_shift5(
-.I_IN1(ab_m1_reg),
-.I_IN2(ab_m2_reg[4:0]),
-.O_OUT(prod_shift)
+.I_IN1(m1_reg),
+.I_IN2(m2_reg[4:0]),
+.O_OUT(prod_shift),
+.O_SFT1(m1_reg_sft5)
     );
 endmodule

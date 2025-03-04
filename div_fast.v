2d79af38c260b43346c787241d42e7e40384e7f7
“lzk2023” <874691437@qq.com>
2025-03-04 20:53:41 +0800

    work 2025_3_4

--- div_fast.v
+++ div_fast.v
@@ -9,11 +9,12 @@ module div_fast#(
 wire              dividend_msb = I_DIVIDEND[D_W-1]  ;//被除数最高位
 wire [D_W-2+13:0] dividend_pos = (dividend_msb ? (~I_DIVIDEND[D_W-2:0]+1):I_DIVIDEND[D_W-2:0]) << 13;//被除数绝对值 扩13位（小数位数量）
 wire              divisor_msb  = I_DIVISOR[D_W-1]   ;//除数最高位
-wire [(D_W-2)*2+13-1:0] divisor_pos  = (divisor_msb ? (~I_DIVISOR[D_W-2:0]+1):I_DIVISOR[D_W-2:0]) << (D_W-3+13);//除数绝对值
+wire [D_W-2:0] divisor_pos = (divisor_msb ? (~I_DIVISOR[D_W-2:0]+1):I_DIVISOR[D_W-2:0]);//除数绝对值
 wire [D_W-1+13:0] quotient_full;
-reg [(D_W-2)*2+13-1:0] divisor_pos_shift;
-reg [D_W-2+13:0] quotient;
-reg [D_W-2+13:0] remainder;
+wire [D_W-2+13:0] quotient = temp_dividend[D_W-2+13:0];
+
+reg [(D_W-1+13)*2-1:0] temp_dividend;
+reg [(D_W-1)+(D_W-1+13)-1:0]temp_divisor;
 
 assign quotient_msb = dividend_msb ^ divisor_msb;
 assign quotient_full[D_W-2+13:0] = (I_DIVIDEND==0) ? 0 : (quotient_msb ? ~quotient + 1 : quotient);
@@ -22,26 +23,14 @@ assign O_QUOTIENT = {quotient_full[D_W-1+13],quotient_full[D_W-2:0]};
 
 integer i;
 always@(*)begin
-    remainder = dividend_pos;
-    for(i=D_W-2+13;i>=0;i=i-1)begin
-        if(i>0)begin
-            if(remainder - divisor_pos_shift > 0)begin
-                quotient[i] = 1;
-                remainder = remainder-divisor_pos_shift;
-            end else begin
-                quotient[i] = 0;
-                remainder = remainder;
-            end
-            divisor_pos_shift = divisor_pos_shift >> 1;
+    temp_dividend = {{(D_W-1+13){1'b0}},dividend_pos};
+    temp_divisor = {divisor_pos,{(D_W-1+13){1'b0}}};
+    for(i=0;i<D_W-1+13;i=i+1)begin
+        temp_dividend = {temp_dividend,1'b0};
+        if(temp_dividend >= temp_divisor)begin
+            temp_dividend = temp_dividend - temp_divisor + 1'b1;
         end else begin
-            if(remainder - divisor_pos_shift > 0)begin
-                quotient[i] = 1;
-                remainder = remainder-divisor_pos_shift;
-            end else begin
-                quotient[i] = 0;
-                remainder = remainder;
-            end
-            divisor_pos_shift = divisor_pos_shift >> 1;
+            temp_dividend = temp_dividend;
         end
     end
 end

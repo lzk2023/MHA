18f1d6b3a0f34900438874a67662828032b7d1ed
lzk2023 <874691437@qq.com>
2025-03-19 17:53:40 +0800

    work_2025_3_19

--- bram_manager.sv
+++ bram_manager.sv
@@ -3,10 +3,13 @@
 module bram_manager(
     input  logic       I_CLK              , 
     input  logic       I_RST_N            , 
-    input  logic       I_VLD_PULSE        ,
-    input  logic [5:0] I_SEL              ,//sel,64 = 256/4
+    input  logic       I_RD_VLD_PULSE     ,
+    input  logic       I_WR_VLD_PULSE     ,
+    input  logic [7:0] I_SEL              ,//sel,addr:{[2:0],[5:0]} high_addr to choose Q,K,V,low_addr to choose line
+    input  logic [7:0] I_MAT [0:15][0:127],
     output logic       O_VLD              ,
-    output logic [7:0] O_MAT [0:15][0:127]
+    output logic [7:0] O_MAT [0:15][0:127],
+    output logic       O_WR_DONE          
 );
 
 enum logic [2:0] {
@@ -14,25 +17,36 @@ enum logic [2:0] {
     S_DELAY  ,
     S_CONCAT0,
     S_CONCAT1,
-    S_OUT    
+    S_OUT    ,
+    S_WR_CONC
 } state;
 
 logic          delay_ff;
 logic [1:0]    sel;
 logic          ena;
+logic          wea;
 logic [9:0]    addra;
+logic [4095:0] dina;
 logic [4095:0] douta;
 logic [7:0]    dout_mat [0:3][0:127];
 
-assign addra = {{2'b0},I_SEL,sel};//{2'b0,[5:0],[1:0]}
+assign addra = {I_SEL,sel};//{2'b0,[5:0],[1:0]}
 bram_ip u_single_port_ram (
     .clka (I_CLK), // input wire clka
     .ena  (ena  ), // input wire ena
-    .wea  (), // input wire [0 : 0] wea
+    .wea  (wea  ), // input wire [0 : 0] wea
     .addra(addra), // input wire [9 : 0] addra
-    .dina (), // input wire [4095 : 0] dina
+    .dina (dina ), // input wire [4095 : 0] dina
     .douta(douta)  // output wire [4095 : 0] douta,delay 2 clks
 );
+
+generate
+    for(genvar i=0;i<4;i=i+1)begin
+        for(genvar j=0;j<128;j=j+1)begin
+            assign dina[(i*128+j)*8 +: 8] = I_MAT[sel*4 + i][j];
+        end
+    end
+endgenerate
 
 generate
     for(genvar x=0;x<4;x=x+1)begin
@@ -41,22 +55,31 @@ generate
         end
     end
 endgenerate
-
+/////////////////FSM///////////////////
 always_ff@(posedge I_CLK or negedge I_RST_N)begin
     if(!I_RST_N)begin
         state <= S_IDLE;
         delay_ff <= 0;
         ena <= 0;
+        wea <= 0;
         sel <= 0;
         O_VLD <= 0;
         O_MAT <= '{default:0};
+        O_WR_DONE <= 0;
     end else begin
         case(state)
             S_IDLE   :begin
-                if(I_VLD_PULSE)begin
+                if(I_WR_VLD_PULSE)begin
+                    state <= S_WR_CONC;
+                    ena <= 1;
+                    wea <= 1;
+                    sel <= 0;
+                    O_WR_DONE <= 0;
+                end else if(I_RD_VLD_PULSE)begin
                     state <= S_DELAY;
                     delay_ff <= 0;
                     ena <= 1;
+                    wea <= 0;
                     sel <= 0;
                     O_VLD <= 0;
                 end else begin
@@ -64,10 +87,17 @@ always_ff@(posedge I_CLK or negedge I_RST_N)begin
                 end
             end
             S_DELAY  :begin
-                if(I_VLD_PULSE)begin
+                if(I_WR_VLD_PULSE)begin
+                    state <= S_WR_CONC;
+                    ena <= 1;
+                    wea <= 1;
+                    sel <= 0;
+                    O_WR_DONE <= 0;
+                end else if(I_RD_VLD_PULSE)begin
                     state <= S_DELAY;
                     delay_ff <= 0;
                     ena <= 1;
+                    wea <= 0;
                     sel <= 0;
                     O_VLD <= 0;
                 end else begin
@@ -82,10 +112,17 @@ always_ff@(posedge I_CLK or negedge I_RST_N)begin
                 end
             end
             S_CONCAT0:begin
-                if(I_VLD_PULSE)begin
+                if(I_WR_VLD_PULSE)begin
+                    state <= S_WR_CONC;
+                    ena <= 1;
+                    wea <= 1;
+                    sel <= 0;
+                    O_WR_DONE <= 0;
+                end else if(I_RD_VLD_PULSE)begin
                     state <= S_DELAY;
                     delay_ff <= 0;
                     ena <= 1;
+                    wea <= 0;
                     sel <= 0;
                     O_VLD <= 0;
                 end else begin
@@ -100,10 +137,17 @@ always_ff@(posedge I_CLK or negedge I_RST_N)begin
                 end
             end
             S_CONCAT1:begin
-                if(I_VLD_PULSE)begin
+                if(I_WR_VLD_PULSE)begin
+                    state <= S_WR_CONC;
+                    ena <= 1;
+                    wea <= 1;
+                    sel <= 0;
+                    O_WR_DONE <= 0;
+                end else if(I_RD_VLD_PULSE)begin
                     state <= S_DELAY;
                     delay_ff <= 0;
                     ena <= 1;
+                    wea <= 0;
                     sel <= 0;
                     O_VLD <= 0;
                 end else begin
@@ -112,17 +156,51 @@ always_ff@(posedge I_CLK or negedge I_RST_N)begin
                 end
             end
             S_OUT    :begin
-                if(I_VLD_PULSE)begin
+                if(I_WR_VLD_PULSE)begin
+                    state <= S_WR_CONC;
+                    ena <= 1;
+                    wea <= 1;
+                    sel <= 0;
+                    O_WR_DONE <= 0;
+                end else if(I_RD_VLD_PULSE)begin
                     state <= S_DELAY;
                     delay_ff <= 0;
                     ena <= 1;
+                    wea <= 0;
                     sel <= 0;
                     O_VLD <= 0;
                 end else begin
                     O_VLD <= 1;
                     ena <= 0;
+                    wea <= 0;
                     O_MAT[12:15] <= dout_mat;
                     state <= S_IDLE;
+                end
+            end
+            S_WR_CONC:begin
+                if(I_WR_VLD_PULSE)begin
+                    state <= S_WR_CONC;
+                    ena <= 1;
+                    wea <= 1;
+                    sel <= 0;
+                    O_WR_DONE <= 0;
+                end else if(I_RD_VLD_PULSE)begin
+                    state <= S_DELAY;
+                    delay_ff <= 0;
+                    ena <= 1;
+                    wea <= 0;
+                    sel <= 0;
+                    O_VLD <= 0;
+                end else begin
+                    if(sel < 2'b11)begin
+                        sel <= sel + 1;
+                        state <= state;
+                    end else begin//sel==3
+                        state <= S_IDLE;
+                        ena <= 0;
+                        wea <= 0;
+                        O_WR_DONE <= 1;
+                    end
                 end
             end
         endcase
--- tb_bram_manager.sv
+++ tb_bram_manager.sv
@@ -1,32 +1,79 @@
 `timescale 1ns/1ps
 module tb_bram_manager();
-bit       I_CLK              ;
-bit       I_RST_N            ;
-bit       I_VLD_PULSE        ;
-bit [5:0] I_SEL              ;
-logic       O_VLD            ;  
+bit         I_CLK              ;
+bit         I_RST_N            ;
+bit         I_RD_VLD_PULSE     ;
+bit         I_WR_VLD_PULSE     ;
+bit [7:0]   I_SEL              ;
+bit [7:0]   I_MAT [0:15][0:127];
+logic       O_VLD              ;  
 logic [7:0] O_MAT [0:15][0:127];
+logic       O_WR_DONE          ;
 bram_manager u_dut(
-    .I_CLK       (I_CLK       ), 
-    .I_RST_N     (I_RST_N     ), 
-    .I_VLD_PULSE (I_VLD_PULSE ),
-    .I_SEL       (I_SEL       ),//sel,64
-    .O_VLD       (O_VLD       ),
-    .O_MAT       (O_MAT       )
+    .I_CLK          (I_CLK          ), 
+    .I_RST_N        (I_RST_N        ), 
+    .I_RD_VLD_PULSE (I_RD_VLD_PULSE ),
+    .I_WR_VLD_PULSE (I_WR_VLD_PULSE ),
+    .I_SEL          (I_SEL          ),//sel,64
+    .I_MAT          (I_MAT          ),
+    .O_VLD          (O_VLD          ),
+    .O_MAT          (O_MAT          ),
+    .O_WR_DONE      (O_WR_DONE      )
 );
 always #5 I_CLK = ~I_CLK;
 initial begin
     #100
     I_RST_N = 1;
-    I_VLD_PULSE = 1;
+    I_RD_VLD_PULSE = 1;
     I_SEL = 0;
     #10
-    I_VLD_PULSE = 0;
-    #200
-    I_VLD_PULSE = 1;
+    I_RD_VLD_PULSE = 0;
+    @(O_VLD)
+    @(posedge I_CLK)
+    I_RD_VLD_PULSE = 1;
     I_SEL = 1;
     #10
-    I_VLD_PULSE = 0;
+    I_RD_VLD_PULSE = 0;
+    @(O_VLD)
+    @(posedge I_CLK)
+    I_RD_VLD_PULSE = 1;
+    I_SEL = 4;
+    #10
+    I_RD_VLD_PULSE = 0;
+
+    @(O_VLD)
+    @(posedge I_CLK)
+    I_WR_VLD_PULSE = 1;
+    I_SEL = 4;
+    I_MAT = '{
+        '{128{8'h55}},
+        '{128{8'h66}},
+        '{128{8'h77}},
+        '{128{8'h88}},
+
+        '{128{8'h55}},
+        '{128{8'h66}},
+        '{128{8'h77}},
+        '{128{8'h88}},
+
+        '{128{8'h55}},
+        '{128{8'h66}},
+        '{128{8'h77}},
+        '{128{8'h88}},
+
+        '{128{8'h55}},
+        '{128{8'h66}},
+        '{128{8'h77}},
+        '{128{8'h88}}
+    };
+    #10
+    I_WR_VLD_PULSE = 0;
+    @(O_WR_DONE)
+    @(posedge I_CLK)
+    I_RD_VLD_PULSE = 1;
+    I_SEL = 4;
+    #10
+    I_RD_VLD_PULSE = 0;
     #200
     $finish;
 end

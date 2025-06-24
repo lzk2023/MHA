8cda460293ce5ab1a300c13f8f442ea96b97a853
“lzk2023” <874691437@qq.com>
2025-06-24 23:44:37 +0800

    add pin

--- FPGA_flash_attn.sv
+++ FPGA_flash_attn.sv
@@ -11,6 +11,7 @@ logic [2:0]    RD_BRAM_COL ;
 logic          BRAM_RD_VLD ;
 logic [4095:0] BRAM_RD_MAT ;
 logic [11:0]   cnt;
+logic [27:0]   cnt_for_sec ;
 
 flash_attn_top u_flash_attn(
     .I_CLK         (I_CLK         ),
@@ -29,42 +30,41 @@ always_ff@(posedge I_CLK or negedge I_RST_N)begin
         RD_BRAM_LINE <= 'b0;
         RD_BRAM_COL  <= 'b0;
         cnt          <= 'b0;
+        cnt_for_sec  <= 'b0;
     end else if(O_ATTN_END)begin
+        cnt_for_sec <= cnt_for_sec + 1;
+        if(cnt_for_sec == 28'hffff_fff)begin
+            cnt <= cnt + 1;
+        end
         if(BRAM_RD_VLD)begin
             RD_BRAM_EN   <= 'b0;
             RD_BRAM_LINE <= RD_BRAM_LINE;
             RD_BRAM_COL  <= RD_BRAM_COL;
-            cnt          <= cnt + 1;
         end else if(cnt == 12'd4095)begin
             if(RD_BRAM_COL == 3'd7)begin
                 if(RD_BRAM_LINE == 6'd63)begin
                     RD_BRAM_EN   <= 'b1;
                     RD_BRAM_LINE <= RD_BRAM_LINE + 1;
                     RD_BRAM_COL  <= RD_BRAM_COL + 1;
-                    cnt          <= cnt + 1;
                 end else begin
                     RD_BRAM_EN   <= 'b1;
                     RD_BRAM_LINE <= RD_BRAM_LINE + 1;
                     RD_BRAM_COL  <= RD_BRAM_COL + 1;
-                    cnt          <= cnt + 1;
                 end
             end else begin
                 RD_BRAM_EN   <= 'b1;
                 RD_BRAM_LINE <= RD_BRAM_LINE;
                 RD_BRAM_COL  <= RD_BRAM_COL + 1;
-                cnt          <= cnt + 1;
             end
         end else begin
             RD_BRAM_EN   <= 'b1;
             RD_BRAM_LINE <= RD_BRAM_LINE;
             RD_BRAM_COL  <= RD_BRAM_COL;
-            cnt          <= cnt + 1;
         end
     end else begin
         RD_BRAM_EN   <= RD_BRAM_EN   ;
         RD_BRAM_LINE <= RD_BRAM_LINE ;
         RD_BRAM_COL  <= RD_BRAM_COL  ;
-        cnt          <= cnt          ;
     end
 end
 assign O_BRAM_RD_1bit = O_ATTN_END ? BRAM_RD_MAT[cnt]: 1'b0;
